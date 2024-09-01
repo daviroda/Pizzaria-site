462447a36b02e601eb3b9d2a2f5e7f7e23b2d32a
daviroda <daviroda48@gmail.com>
2024-09-01 19:45:08 -0300

    Site no ar
    Finalização da landing page.

---
+++ script/main.js
@@ -0,0 +1,16 @@
+//script para resolver bug do menu
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
+
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
